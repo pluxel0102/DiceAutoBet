7cf67cc666124578701ddef47ddac8edb09cd1bb
pluxel0102 <pluxel0102@example.com>
2025-10-21 00:01:17 +0300

    Reset version to 1.0.0 for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.diceautobet"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
